431619aeb9eb4cf30b690410a1b082f47f8d90bf
Guillaume Marty <edo999@gmail.com>
2014-03-04 15:06:39 +0000

    Use placeholders to generate more idiomatic translations

--- build-pos-converter.js
+++ build-pos-converter.js
@@ -17,12 +17,18 @@
 
 var fs = require('fs');
 var pos = require('pos');
+var _ = require('lodash');
 
 var jaCorpus = ('' + fs.readFileSync('./corpus/ja-segmented.txt', {encoding: 'utf8'})).split('\r\n');
 var enCorpus = ('' + fs.readFileSync('./corpus/en.txt', {encoding: 'utf8'})).split('\r\n');
 
 var jaPos = require('./corpus-tagged/ja.json');
 var enPos = require('./corpus-tagged/en.json');
+
+// The POS common to both Chasen and js-pos.
+var posItems = ["UH", "RB", "JJ", "NN", "NNP", "CD", "SYM", ".", ":", "(", ")", "VB", "CC"];
+
+var lexer = new pos.Lexer();
 
 var jaConverter = {};
 var enConverter = {};
@@ -34,8 +40,9 @@ for (var i = 0; i < length; i++) {
   // Expand the Japanese POS converter.
   if (!jaConverter[jaPos[i]]) {
     jaConverter[jaPos[i]] = {
-      en: {},
-      commons: Array(jaPos[i].split('|').length)
+      'en': {},
+      'commons': Array(jaPos[i].split('|').length),
+      'p': []
     };
   }
   // Add English matches to the list.
@@ -46,8 +53,8 @@ for (var i = 0; i < length; i++) {
   // Add Japanese commons to the list.
   sentence = jaCorpus[i];
   sentence = sentence.replace(/\|$/, ''); // Remove the trailing pipe.
-  sentence = sentence.split('|');
-  sentence.forEach(function(word, index) {
+  var words = sentence.split('|');
+  words.forEach(function(word, index) {
     if (!jaConverter[jaPos[i]]['commons'][index]) {
       jaConverter[jaPos[i]]['commons'][index] = {};
     }
@@ -56,12 +63,14 @@ for (var i = 0; i < length; i++) {
     }
     jaConverter[jaPos[i]]['commons'][index][word]++;
   });
+  jaConverter[jaPos[i]]['p'].push(sentence);
 
   // Expand the English POS converter.
   if (!enConverter[enPos[i]]) {
     enConverter[enPos[i]] = {
-      ja: {},
-      commons: Array(enPos[i].split('|').length)
+      'ja': {},
+      'commons': Array(enPos[i].split('|').length),
+      'p': []
     };
   }
   // Add Japanese matches to the list.
@@ -73,8 +82,8 @@ for (var i = 0; i < length; i++) {
   sentence = enCorpus[i];
   sentence = sentence.trim().replace(/(\.|\?|!|:)+$/, ''); // Remove the trailing punctuation.
   //sentence = sentence.replace(/(\.|;|,|:)+$/, ''); // Remove punctuation.
-  sentence = new pos.Lexer().lex(sentence);
-  sentence.forEach(function(word, index) {
+  var words = lexer.lex(sentence);
+  words.forEach(function(word, index) {
     if (!enConverter[enPos[i]]['commons'][index]) {
       enConverter[enPos[i]]['commons'][index] = {};
     }
@@ -83,6 +92,121 @@ for (var i = 0; i < length; i++) {
     }
     enConverter[enPos[i]]['commons'][index][word]++;
   });
+  enConverter[enPos[i]]['p'].push(sentence);
+}
+
+// Pass 2, filling in the placeholders.
+// The engine should be able to manage replace POS item in common in both corpus. So we need a way
+// to get the most common structure for POS items non existing in one or the other (P.c....).
+
+// Generate the Japanese placeholder string used in English to Japanese translation.
+for (var i in jaConverter) {
+  // For each commons, find the more frequent item.
+  jaConverter[i]['commons'] = jaConverter[i]['commons'].map(function(terms) {
+    var term = '';
+    var value = 0;
+    for (var j in terms) {
+      if (terms[j] > value) {
+        term = j;
+        value = terms[j];
+      }
+    }
+    return term;
+  });
+
+  // Generate the placeholders.
+  jaConverter[i]['p'] = jaConverter[i]['p'].map(function(sentence) {
+    return sentence.split('|');
+  });
+
+  var pos = i.split('|');
+
+  pos.forEach(function(posItem, index) {
+    if (posItems.indexOf(posItem) > 0) {
+      jaConverter[i]['p'] = jaConverter[i]['p'].map(function(samples) {
+        samples[index] = '@@@';
+        return samples;
+      });
+    }
+  });
+
+  jaConverter[i]['p'] = jaConverter[i]['p'].map(function(sentence) {
+    return sentence.join('|');
+  });
+  jaConverter[i]['p'] = _.countBy(jaConverter[i]['p'], function(num) {
+    return num;
+  });
+  var placeholders = {};
+  for (var j in jaConverter[i]['p']) {
+    pos = j.split('|');
+    pos = pos.map(function(posItem, index) {
+      if (posItem === '@@@') {
+        return jaConverter[i]['commons'][index];
+      }
+      return posItem;
+    });
+    pos = pos.join('|');
+    placeholders[pos] = jaConverter[i]['p'][j];
+  }
+  jaConverter[i]['p'] = placeholders;
+
+  // From here, we don't need common anymore.
+  delete jaConverter[i]['commons'];
+}
+
+// Generate the English placeholder string used in Japanese to English translation.
+for (var i in enConverter) {
+  // For each commons, find the more frequent item.
+  enConverter[i]['commons'] = enConverter[i]['commons'].map(function(terms) {
+    var term = '';
+    var value = 0;
+    for (var j in terms) {
+      if (terms[j] > value) {
+        term = j;
+        value = terms[j];
+      }
+    }
+    return term;
+  });
+
+  // Generate the placeholders.
+  enConverter[i]['p'] = enConverter[i]['p'].map(function(sentence) {
+    return lexer.lex(sentence);
+  });
+
+  var pos = i.split('|');
+
+  pos.forEach(function(posItem, index) {
+    if (posItems.indexOf(posItem) > 0) {
+      enConverter[i]['p'] = enConverter[i]['p'].map(function(samples) {
+        samples[index] = '@@@';
+        return samples;
+      });
+    }
+  });
+
+  enConverter[i]['p'] = enConverter[i]['p'].map(function(sentence) {
+    return sentence.join('|');
+  });
+  enConverter[i]['p'] = _.countBy(enConverter[i]['p'], function(num) {
+    return num;
+  });
+  var placeholders = {};
+  for (var j in enConverter[i]['p']) {
+    pos = j.split('|');
+    pos = pos.map(function(posItem, index) {
+      if (posItem === '@@@') {
+        return enConverter[i]['commons'][index];
+      }
+      return posItem;
+    });
+    pos = pos.join('|');
+    placeholders[pos] = enConverter[i]['p'][j];
+  }
+  enConverter[i]['p'] = placeholders;
+
+  // From here, we don't need common anymore.
+  delete enConverter[i]['commons'];
 }
 
 fs.writeFileSync('./pos-converter/ja.json', JSON.stringify(jaConverter, null, '  '), {encoding: 'utf8'});
